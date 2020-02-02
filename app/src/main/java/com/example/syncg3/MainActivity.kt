69754ac0dbfb57c8520fc32a29ba55ca28f45900
Dru <andru.rosen@gmail.com>
2020-02-02 17:42:22 -0500

    Set items next to labels

--- app/src/main/java/com/example/syncg3/MainActivity.kt
+++ app/src/main/java/com/example/syncg3/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.syncg3
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +32,22 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val inflater = layoutInflater
+        val navigationMenuView = navView.getChildAt(0) as BottomNavigationMenuView
+        val menu: Menu = navView.menu
+        for (i in 0 until menu.size()) {
+            val view =
+                navigationMenuView.getChildAt(i) as BottomNavigationItemView
+            val itemBottomNavigation: View =
+                inflater.inflate(R.layout.item_bottom_navigation_bar, null, false)
+            (itemBottomNavigation.findViewById<View>(R.id.icon) as ImageView).setImageDrawable(
+                menu.getItem(i).icon
+            )
+            (itemBottomNavigation.findViewById<View>(R.id.title) as TextView).text =
+                menu.getItem(i).title
+            view.removeAllViews()
+            view.addView(itemBottomNavigation)
+        }
     }
 }
